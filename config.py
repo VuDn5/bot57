9bbbc976c932649fc98fd08e5c52176a8fc05dc0
VuDn5 <36143595+VuDn5@users.noreply.github.com>
2021-11-30 13:21:19 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = "ca16c5a2-0120-4edd-a0d9-ec49a21e1df1"
+    APP_PASSWORD = "Ma77Q~JsOl~YmnE-4RdANKpEguovyzpLXe9jC"
+    """
+    APP_ID = os.environ.get("")
+    APP_PASSWORD = os.environ.get("")
+    """
